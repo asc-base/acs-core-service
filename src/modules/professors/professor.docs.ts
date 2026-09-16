212566d898a220af9a9cf54208ec7b5575763d6f
orporpanggg <ppc.opal@gmail.com>
2026-09-16 19:59:04 +0700

    fix: replace commas with slashes in expertFields and educations during professor update

--- src/modules/professors/professor.docs.ts
+++ src/modules/professors/professor.docs.ts
@@ -65,6 +65,13 @@ export const ProfessorDocs = {
       tags: ["Professors"],
     },
     transform({ body }: { body: ProfessorUpdateDTO }) {
+      if (body.expertFields != null && body.expertFields !== "") {
+        body.expertFields = body.expertFields.replaceAll(",", "/");
+      }
+      if (body.educations != null && body.educations !== "") {
+        body.educations = body.educations.replaceAll(",", "/");
+      }
+
       Object.keys(body).forEach((key) => {
         const k = key as keyof ProfessorUpdateDTO;
         if (body[k] === "") {
