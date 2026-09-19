1768bd03c298c213563f245669c1f93ab93577ea
chanatip keawchaoon <charnatip15@gmail.com>
2026-09-20 00:40:20 +0700

    restore lost migration

---
+++ prisma/migrations/20260818183515_add_news_features_focal_points/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "news_features" ADD COLUMN     "highlight_focal_point_x" DOUBLE PRECISION,
+ADD COLUMN     "highlight_focal_point_y" DOUBLE PRECISION,
+ADD COLUMN     "thumbnail_focal_point_x" DOUBLE PRECISION,
+ADD COLUMN     "thumbnail_focal_point_y" DOUBLE PRECISION;
